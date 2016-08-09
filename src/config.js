d4c8d5cd7c453b6f7bd9ea05d91e451108177ea0
tmhammer <timh@govwizely.com>
2016-08-09 16:00:30 -0400

    Update key

--- src/config.js
+++ src/config.js
@@ -13,7 +13,7 @@ const config = Object.assign({
     api: {
       i94: {
         host: 'https://api.govwizely.com/v1/i94_monthly_data/search',
-        apiKey: 'hQ4L7ylI9POH3QuqxOY_l2UC',
+        apiKey: 'Z48wSr3E3nNN4itDUvE4Clje',
       },
     },
   },
